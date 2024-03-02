e5dd698065baad66d67693b7a464c4ea4d92f219
Brady Kelly <brady@bradykelly.net>
2024-03-02 21:25:17 +0200

    Busy on reading files

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,14 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const commander_1 = require("commander");
+const figlet = require('figlet');
+const program = new commander_1.Command();
+// TODO Get auto-version extension working
+program
+    .version('0.0.1')
+    .description('Generate TypeScript interfaces from a JSON Schema')
+    .option('-i, --input <input>', 'Path to input JSON Schema file')
+    .option('-o, --output <output>', 'Path to output TypeScript file')
+    .parse(process.argv);
+console.log(figlet.textSync('Schema-to-ts', { horizontalLayout: 'full' }));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,51 @@
+import { Command } from "commander";
+import { compile, compileFromFile } from "json-schema-to-typescript";
+import * as fs from "fs";
+
 const figlet = require('figlet');
+const program = new Command()
+
+// TODO Get auto-version extension working
+program
+    .version('0.0.1')
+    .description('Generate TypeScript interfaces from a JSON Schema')
+    .option('-i, --input <input>', 'Path to input JSON Schema file')
+    .option('-o, --output <output>', 'Path to output TypeScript file')
+    .option('-d, --debug', 'Enable debug logging')
+    .parse(process.argv)
+   
+    const fsPromises = fs.promises
+    const options = program.opts()
+    let schemaText = ''
+
+    fsPromises.readFile(options.input)
+        .then((data) => {
+            schemaText = data.toString()
+                 console.log('Schema read from'+ options.input)
+         })
+         .catch((err) => {
+            console.error(err)
+            return;
+          })
+         }
+        const schema = JSON.parse(data.toString())
+             const ts = generateInterface(schema)
+             fs.writeFile(options.output, ts, (err) => {
+                 if (err) throw err;
+                 console.log('Schema written to'+ options.output)
+             })
+        })
+    // TODO Only log if dev or debug mode
+    console.log('Schema read from'+ options.input)
+
+    const schema = JSON.parse(data.toString())
+    const ts = generateInterface(schema)
+    fs.writeFile(options.output, ts, (err) => {
+        if (err) throw err;
+        console.log('Schema written to'+ options.output)
+    })
+})
+}
+
 
 console.log(figlet.textSync('Schema-to-ts', { horizontalLayout: 'full' }));
